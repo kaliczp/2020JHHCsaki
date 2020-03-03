952d98de6d700db52bc40e5d751f6224969ed02d
Péter Kalicz <kaliczp@uni-sopron.hu>
2020-03-03 17:17:16 +0100

    Külön objektumból boxplot
    A boxplot kirajzolása előtt elmentem az objektumot, ami az adatokat
    tartalmazza. Abból csinálom a boxplotot.

--- Zala_reszviz_boxplotok.R
+++ Zala_reszviz_boxplotok.R
@@ -3,9 +3,10 @@
 Zala_ET = read.csv2("Zala_reszviz_ET.csv",sep = ";", header=FALSE, dec=".")
 ##az elso 4 sor az elso idoszak a 4 tájra, stb.
 ##beta-redukalt eredmenyek!
+ETdata <- t(Zala_ET)
 jpeg("Zala_ET.jpg",16,7,units="cm",res=1000,pointsize = 8)
 par(mar=c(3,4.1,1.1,1.1))
-boxplot(t(Zala_ET), #names=c(" ","2011-2040"," "," ","2041-2070"," "," ","2071-2100"," "),
+boxplot(ETdata, #names=c(" ","2011-2040"," "," ","2041-2070"," "," ","2071-2100"," "),
         ylab="ET-change [%]",boxwex=0.5,
         ylim=c(-11,14), col= c("#2b83ba","#abdda4","#fdae61","#d7191c",
                                "#2b83ba","#abdda4","#fdae61","#d7191c",
@@ -33,9 +34,10 @@ dev.off()
 Zala_R = read.csv2("Zala_reszviz_R.csv",sep = ";", header=FALSE, dec=".")
 ##az elso 4 sor az elso idoszak a 4 tájra, stb.
 ##beta-redukalt eredmenyek!
+ZalaRdata <- t(Zala_R)
 jpeg("Zala_R.jpg",16,7,units="cm",res=1000,pointsize = 8)
 par(mar=c(3,4.1,1.1,1.1))
-boxplot(t(Zala_R), #names=c(" ","2011-2040"," "," ","2041-2070"," "," ","2071-2100"," "),
+boxplot(ZalaRdata, #names=c(" ","2011-2040"," "," ","2041-2070"," "," ","2071-2100"," "),
         ylab="R-change [%]",boxwex=0.5,
         ylim=c(-110,50), col= c("#2b83ba","#abdda4","#fdae61","#d7191c",
                                "#2b83ba","#abdda4","#fdae61","#d7191c",
